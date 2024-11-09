e2a997f9ac46a794aeac563a2a794527f87bb28c
Jakub Klimczewski <kukli8@o2.pl>
2024-11-09 11:09:47 +0100

    Fixed healthcheck

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth';
 import KeycloakProvider from 'next-auth/providers/keycloak';
+import { env } from 'process';
 
-const clientId = process.env.KEYCLOAK_CLIENT_ID || '';
-const clientSecret = process.env.KEYCLOAK_CLIENT_SECRET || '';
-const issuer = process.env.KEYCLOAK_ISSUER || '';
+const clientId = env.KEYCLOAK_CLIENT_ID || '';
+const clientSecret = env.KEYCLOAK_CLIENT_SECRET || '';
+const issuer = env.KEYCLOAK_ISSUER || '';
 
 export const authOptions = {
   providers: [
