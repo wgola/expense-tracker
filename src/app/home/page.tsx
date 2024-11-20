d048aad7ff8423154cf11b61dd0af88fa6243783
Jakub Klimczewski <kukli8@o2.pl>
2024-11-20 22:11:29 +0100

    Added conditional user view

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,21 @@
+'use client';
+
 import Logout from '@/components/logout';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
-  return (
-    <div>
-      Home
-      <Logout />
-    </div>
-  );
+  const { data: session, status } = useSession();
+  //   const session = await getServerSession(authOptions);
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  } else if (status === 'unauthenticated') {
+    return <p>Access denied. You are not authorized to view this page.</p>;
+  } else {
+    return (
+      <div>
+        <h1>Hello, {session?.user?.name || 'User'}!</h1>
+        <Logout />
+      </div>
+    );
+  }
 }
