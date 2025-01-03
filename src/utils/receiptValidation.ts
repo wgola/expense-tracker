cadb4b6ce86118514c43b5d479e0643932351a15
Jakub Klimczewski <kukli8@o2.pl>
2025-01-03 12:17:16 +0100

    Data validation changes and form default values

--- src/utils/receiptValidation.ts
+++ src/utils/receiptValidation.ts
@@ -2,9 +2,11 @@ import { z } from 'zod';
 
 export const receiptSchema = z.object({
   owner: z.string(),
-  name: z.string().min(3),
+  name: z.string({ message: 'Name is required' }).min(3, 'Name must be at least 3 characters long'),
   imageName: z.string(),
-  category: z.string().min(3),
+  category: z
+    .string({ message: 'Category is required' })
+    .min(3, 'Category must be at least 3 characters long'),
   date: z.date().refine((date) => date <= new Date(), {
     message: 'Date should be from the past'
   }),
